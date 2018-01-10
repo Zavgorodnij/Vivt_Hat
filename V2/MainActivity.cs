63a1b7a87af4c4ade5723afc28ae82b74d72bf70
Evgenij Zavgorodnij <razorval99@gmail.com>
2018-01-10 23:38:10 +0300

    Add files via upload

---
+++ V2/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using Android.Media;
+using Java.Lang;
+using Android.Views;
+
+namespace App4
+{
+    [Activity(Label = "VIVT Hat(Manual)", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public static MediaPlayer player;
+        Button button1, button2;
+        protected override void OnStop()
+        {
+            base.OnStop();
+            if (MainActivity.player != null && MainActivity.player.IsPlaying) MainActivity.player.Stop();
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            button1 = FindViewById<Button>(Resource.Id.button1);
+            button1.Click += delegate
+             {
+                 Intent intent = new Intent(this, typeof(AutoActivity));
+
+                 StartActivity(intent);
+
+             };
+            button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += delegate
+              {
+                  Intent intent = new Intent(this, typeof(ManualActivity));
+
+                  StartActivity(intent);
+
+              };
+        }
+        
+    }
+
+}
+
---
+++ V2/ManualActivity.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using Android.Media;
+using Java.Lang;
+
+namespace App4
+{
+    [Activity(Label = "VIVT Hat")]
+    class ManualActivity: Activity
+    {
+        Button button1, button2, button3, button4, button5, button6, button7, button8;
+        int start = 0;
+        int second = 3000;
+        protected override void OnStop()
+        {
+            base.OnStop();
+            if (MainActivity.player != null && MainActivity.player.IsPlaying) MainActivity.player.Stop();
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ManualActivity);
+            //Buttom
+            button1 = FindViewById<Button>(Resource.Id.buttonM1);
+            button1.Click += delegate
+            {
+                Random random = new Random();
+                int idTrack = 0;
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        idTrack = Resource.Raw.yp;
+                        break;
+                    case 2:
+                        start++;
+                        idTrack = Resource.Raw.pks;
+                        break;
+                    default:
+                        start++;
+
+
+                        break;
+                }
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+
+            };
+            button2 = FindViewById<Button>(Resource.Id.buttonM2);
+            button2.Click += delegate
+            {
+                Random random = new Random();
+                int idTrack = 0;
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        idTrack = Resource.Raw.yp;
+                        break;
+                    case 2:
+                        start++;
+                        idTrack = Resource.Raw.pks;
+                        break;
+                    default:
+                        start++;
+
+
+                        break;
+                }
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+            };
+            button3 = FindViewById<Button>(Resource.Id.buttonM3);
+            button3.Click += delegate
+            {
+                Random random = new Random();
+                int idTrack=0;
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        idTrack = Resource.Raw.yp;
+                        break;
+                    case 2:
+                        start++;
+                        idTrack = Resource.Raw.pks;
+                        break;
+                    default:
+                        start++;
+                       
+                        
+                        break;
+                }
+                
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+            };
+            button4 = FindViewById<Button>(Resource.Id.buttonM4);
+            button4.Click += delegate
+            {
+                Random random = new Random();
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        MainActivity.player = MediaPlayer.Create(this, Resource.Raw.yp);
+                        MainActivity.player.Start();
+                        Thread.Sleep(second);
+                        break;
+                    case 2:
+                        start++;
+                        MainActivity.player = MediaPlayer.Create(this, Resource.Raw.pks);
+                        MainActivity.player.Start();
+                        Thread.Sleep(second);
+                        break;
+                    default:
+                        start++;
+                        MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                        MainActivity.player.Start();
+                        Thread.Sleep(second);
+                        break;
+                }
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+            };
+            button5 = FindViewById<Button>(Resource.Id.buttonM5);
+            button5.Click += delegate
+            {
+                Random random = new Random();
+                int idTrack = 0;
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        idTrack = Resource.Raw.yp;
+                        break;
+                    case 2:
+                        start++;
+                        idTrack = Resource.Raw.pks;
+                        break;
+                    default:
+                        start++;
+
+
+                        break;
+                }
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+            };
+            button6 = FindViewById<Button>(Resource.Id.buttonM6);
+            button6.Click += delegate
+            {
+                Random random = new Random();
+                int idTrack = 0;
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        idTrack = Resource.Raw.yp;
+                        break;
+                    case 2:
+                        start++;
+                        idTrack = Resource.Raw.pks;
+                        break;
+                    default:
+                        start++;
+
+
+                        break;
+                }
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+            };
+            button7 = FindViewById<Button>(Resource.Id.buttonM7);
+            button7.Click += delegate
+            {
+                Random random = new Random();
+                int idTrack = 0;
+                switch (random.Next(0, 3))
+                {
+                    case 1:
+                        start++;
+                        idTrack = Resource.Raw.yp;
+                        break;
+                    case 2:
+                        start++;
+                        idTrack = Resource.Raw.pks;
+                        break;
+                    default:
+                        start++;
+
+
+                        break;
+                }
+                MainActivity.player = MediaPlayer.Create(this, Resource.Raw.Addit);
+                MainActivity.player.Start();
+                Thread.Sleep(second);
+            };
+            button8 = FindViewById<Button>(Resource.Id.buttonM8);
+            button8.Click += delegate
+            {
+                if (start>0)
+                {
+                    MainActivity.player.Stop();
+                }
+                Intent intent = new Intent(this, typeof(MainActivity));
+                StartActivity(intent);
+                
+            };
+        }
+    }
+}
